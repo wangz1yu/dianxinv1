cb7e314aea99d5d9c6d3bd81e442b1ff65eca44b
wangz1yu <49710008+wangz1yu@users.noreply.github.com>
2026-03-03 02:00:10 +0000

    修复

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Home from '@/pages/Home';
