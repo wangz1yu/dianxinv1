ee3607d104306917812b1de6d22fd9d7d9ee82e5
wangz1yu <49710008+wangz1yu@users.noreply.github.com>
2026-04-03 03:54:32 +0000

    修复视频

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -125,7 +125,7 @@ export default function Hero() {
       </div>
 
       {/* Bottom hero video */}
-      <div className="absolute left-0 right-0 bottom-0 h-72 md:h-80 lg:h-96 overflow-hidden z-0">
+      <div className="relative w-full mt-8 overflow-hidden rounded-3xl h-72 md:h-80 lg:h-96 z-0">
         <video
           src="https://oss.dianxin.love/hero.mp4"
           autoPlay
@@ -138,7 +138,7 @@ export default function Hero() {
       </div>
 
       {/* Bottom Gradient Fade */}
-      <div className="absolute left-0 right-0 bottom-0 h-32 bg-gradient-to-t from-[#0A1628] via-[#0d1f35] to-transparent z-10" />
+      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-[#0A1628] via-[#0d1f35] to-transparent z-10" />
     </section>
   );
 }
